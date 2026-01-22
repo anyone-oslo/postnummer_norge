86bd4e18600ebcf25135583fd31ae6c220a305ea
Inge Jørgensen <inge@elektronaut.no>
2026-01-23 00:20:55 +0100

    refactor: Switch from VERSION file to version.rb

--- lib/postnummer_norge/version.rb
+++ lib/postnummer_norge/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module PostnummerNorge
-  VERSION = File.read(File.expand_path("../../VERSION", __dir__)).strip unless PostnummerNorge.const_defined?(:VERSION)
+  VERSION = "1.0.0"
 end
